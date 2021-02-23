7f32157fbd8879741245ff70793c9d12b0c0cf2b
venguru <kin.sh0717@gmail.com>
2021-02-23 16:45:59 +0900

    first commit

---
+++ test/index.js
@@ -0,0 +1,21 @@
+const assert = require("assert");
+const fizzbuzz = require("../index");
+
+describe("fizzbuzz", () => {
+    it("returns FizzBuzz whne value is divisible by 15",
+    () => {
+        assert(fizzbuzz(30) === "FizzBuzz");
+    });
+
+    it("return Fizz when value is divisible by 3", () => {
+        assert(fizzbuzz(9) === "Fizz");
+    });
+
+    it("returns Buzz when value is divisible by 5", () => {
+        assert(fizzbuzz(10) === "Buzz");
+    });
+
+    it("returns the value when value is not divisible by 3 or 5", () => {
+        assert(fizzbuzz(7) === "7");
+    });
+});
